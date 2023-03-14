25876c6405c0ad6f160aa0121339ba7369ce73b9
OrhanMA <orhan.madi.assani@gmail.com>
2023-03-14 13:45:49 +0100

    check answer on buttons

--- src/script.ts
+++ src/script.ts
@@ -4,70 +4,45 @@ console.log("test");
 
 interface Question {
   question: string;
-  choice1: string;
-  choice2: string;
-  choice3: string;
-  choice4: string;
-  id: number;
+  options: string[];
 }
 
 const question1: Question = {
   question: "Q.1 Which continent is China in?",
-  choice1: "Africa",
-  choice2: "Americas",
-  choice3: "Europe",
-  choice4: "Asia",
-  id: 1,
+  options: ["Africa", "Americas", "Europe", "Asia"],
 };
 const question2: Question = {
   question: "Q.2 What is the capital of China?",
-  choice1: "Beijing",
-  choice2: "Pyongyqng",
-  choice3: "Shanghai",
-  choice4: "Hongkong",
-  id: 2,
+  options: ["Beijing", "Pyongyang", "Shanghai", "Hongkong"],
 };
 const question3: Question = {
   question: "Q.3 What is China's major export?",
-  choice1: "Machinery",
-  choice2: "Electronics",
-  choice3: "Rice",
-  choice4: "Materials",
-  id: 3,
+  options: ["Machinery", "Electronics", "Rice", "Materials"],
 };
 const question4: Question = {
   question: "Q.4 What is China's total population (in billions)?",
-  choice1: "1.4",
-  choice2: "24",
-  choice3: "2.8",
-  choice4: "2",
-  id: 4,
+  options: ["1.4", "24", "2.8", "2"],
 };
 const question5: Question = {
   question: "Q.5 What is China's most popular religion?",
-  choice1: "Catholicism",
-  choice2: "Buddhism",
-  choice3: "Christianity",
-  choice4: "Islam",
-  id: 5,
+  options: ["Catholicism", "Buddhism", "Christianity", "Islam"],
 };
 
+const main = document.querySelector(".main") as HTMLDivElement;
 // selecting a div to make appear the question in
-const content = document.querySelector(".content") as HTMLDivElement;
+const content = document.createElement("div") as HTMLDivElement;
 content.classList.add("content");
+main.appendChild(content);
 
 function displayQuestion(
+  div: HTMLDivquestion,
   question: interface,
-  answer1: string,
-  answer2: string,
-  answer3: string,
-  answer4: string,
-  id: number
+  options: string[]
 ) {
   // container for the question elements
   const container = document.createElement("div") as HTMLDivElement;
   container.classList.add("container");
-  content.appendChild(container);
+  div.appendChild(container);
   // creating the question's element
   const currentQuestion = document.createElement("h2") as HTMLHeadingElement;
   currentQuestion.textContent = question.question;
@@ -78,25 +53,19 @@ function displayQuestion(
   choicesContainer.classList.add("choices-container");
   container.appendChild(choicesContainer);
   // creating each answers a function
-  createAnswer(answer1);
-  createAnswer(answer2);
-  createAnswer(answer3);
-  createAnswer(answer4);
-  const confirmBtn = document.createElement("button") as HTMLButtonElement;
-  confirmBtn.textContent = "Confirm";
-  confirmBtn.classList.add("confirm-button");
-  confirmBtn.classList.add(`${id}`);
-  container.appendChild(confirmBtn);
+  createAnswer(options);
 }
 
-function createAnswer(answer: string) {
+function createAnswer(options: string[]) {
   const choicesContainer = document.querySelector(
     ".choices-container"
   ) as HTMLDivElement;
-  const answerBtn = document.createElement("button") as HTMLButtonElement;
-  answerBtn.textContent = answer;
-  answerBtn.classList.add(`${answer}`);
-  choicesContainer.appendChild(answerBtn);
+  for (let i = 0; i < options.length; i++) {
+    const answerBtn = document.createElement("button") as HTMLButtonElement;
+    answerBtn.textContent = `${options[i]}`;
+    answerBtn.classList.add(`${options.indexOf(options[i])}`);
+    choicesContainer.appendChild(answerBtn);
+  }
 }
 
 const presentation = document.querySelector(".presentation") as HTMLDivElement;
@@ -105,64 +74,27 @@ content.style.display = "none";
 startBtn.addEventListener("click", () => {
   presentation.style.display = "none";
   content.style.display = "flex";
-  displayQuestion(
-    question1,
-    question1.choice1,
-    question1.choice2,
-    question1.choice3,
-    question1.choice4,
-    question1.id
-  );
-  const confirmBtn = document.querySelector(
-    ".confirm-button"
-  ) as HTMLButtonElement;
-  confirmBtn.addEventListener("click", () => {
-    if (confirmBtn.classList.contains("1")) {
-      const container = document.querySelector(".container") as HTMLDivElement;
-      container.style.display = "none";
-      displayQuestion(
-        question2,
-        question2.choice1,
-        question2.choice2,
-        question2.choice3,
-        question2.choice4,
-        question2.id
-      );
-    }
-  });
+  displayQuestion(content, question1, question1.options);
+  const buttons = document.getElementsByTagName("button");
+  console.log(buttons);
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", () => {
+      console.log(buttons[i]);
+      if (buttons[i].classList.contains("3")) {
+        console.log(`${buttons[i].textContent} is the right answer`);
+        userAnswers.push("3");
+      } else if (buttons[i].textContent === "Click to start playing") {
+        console.log("does not apply");
+      } else {
+        console.log(`${buttons[i].textContent} is not the right answer.`);
+        userAnswers.push(`${buttons[i].classList}`);
+      }
+      console.log(userAnswers);
+    });
+  }
 });
 
-function displayNext() {}
-/* displayQuestion(
-  question2,
-  question2.choice1,
-  question2.choice2,
-  question2.choice3,
-  question2.choice4,
-  question2.id
-);
-displayQuestion(
-  question3,
-  question3.choice1,
-  question3.choice2,
-  question3.choice3,
-  question3.choice4,
-  question3.id
-);
-displayQuestion(
-  question4,
-  question4.choice1,
-  question4.choice2,
-  question4.choice3,
-  question4.choice4,
-  question4.id
-);
-displayQuestion(
-  question5,
-  question5.choice1,
-  question5.choice2,
-  question5.choice3,
-  question5.choice4,
-  question5.id
-);
- */
+const userAnswers: string[] = [];
+const rightAnswers: string[] = ["4", "1", "2", "1", "2"];
+
+function checkAnswer() {}
