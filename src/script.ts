9fd5f4e9f51ce2d285f2be70e6c3eaecbd52545d
OrhanMA <orhan.madi.assani@gmail.com>
2023-03-21 01:37:55 +0100

    added nba version

--- src/script.ts
+++ src/script.ts
@@ -1,270 +1,141 @@
 "use strict";
 
-console.log("test");
+let numberGoodAnswer: number = 0;
+let questionIndex: number = 0;
+let playerPoints: number = 0;
+let secondsTimer: number = 20;
 
-interface Question {
-  question: string;
-  options: string[];
-  correctAnswer: string;
-  image: string;
-}
+const presentation = document.querySelector(".presentation") as HTMLDivElement;
+const questionDiv = document.querySelector(".questionDiv") as HTMLDivElement;
+const answerDiv = document.querySelector(".answerDiv") as HTMLDivElement;
+const finalResultDiv = document.querySelector(".finalResult") as HTMLDivElement;
 
-const questions: Question[] = [
+//Initally hidden stuff
+questionDiv.style.display = "none";
+answerDiv.style.display = "none";
+finalResultDiv.style.display = "none";
+
+const questionArray = [
   {
-    question: "Q.1 Which continent is China in?",
-    options: ["Africa", "Americas", "Europe", "Asia"],
-    correctAnswer: "Asia",
-    image: "../assets/asia.png",
+    question:
+      "Who's the player that joined the Golden State Warriors in 2017 and formed the greatest basketball team of all times?",
+    options: ["Lebron James", "Kevin Durant", "Jayson Tatum", "Devin Booker"],
+    correctAnswer: "Kevin Durant",
   },
   {
-    question: "Q.2 What is the capital of China?",
-    options: ["Beijing", "Pyongyang", "Shanghai", "Hongkong"],
-    correctAnswer: "Beijing",
-    image: "../assets/beijing.jpeg",
+    question: "Who's the 2011 MVP?",
+    options: ["Dwight Howard", "Derrick Rose", "Lebron James", "Blake Griffin"],
+    correctAnswer: "Derrick Rose",
   },
   {
-    question: "Q.3 What is China's major export?",
-    options: ["Machinery", "Electronics", "Rice", "Materials"],
-    correctAnswer: "Electronics",
-    image: "../assets/usine.jpeg",
+    question:
+      "Who's the player that won the most championships in NBA History? ",
+    options: ["Micheal Jordan", "Bill Russell", "Sam Jones", "Larry Bird"],
+    correctAnswer: "Bill Russell",
   },
   {
-    question: "Q.4 What is China's total population (in billions)?",
-    options: ["1.4", "24", "2.8", "2"],
-    correctAnswer: "1.4",
-    image: "../assets/monument.jpg",
+    question: "In which year did Kobe Bryant retired?",
+    options: ["2009", "2012", "2016", "2015"],
+    correctAnswer: "2016",
   },
   {
-    question: "Q.5 What is China's most popular religion?",
-    options: ["Catholicism", "Buddhism", "Christianity", "Islam"],
-    correctAnswer: "Buddhism",
-    image: "../assets/buddhism.jpg",
+    question: "Which nba player is also called 'Uncle Drew' ?",
+    options: ["John Drew", "Dwyane Wade", "Tony Parker", "Kyrie Irving"],
+    correctAnswer: "Kyrie Irving",
   },
 ];
 
-let currentQuestionIndex: number = 0;
-let score: number = 0;
-const questionContainer = document.querySelector(
-  ".question-container"
-) as HTMLDivElement;
-
-const questionElement = document.querySelector(".question") as HTMLDivElement;
-const buttonDiv = document.createElement("div") as HTMLDivElement;
-buttonDiv.classList.add("buttonDiv");
-const button1 = document.querySelector("#button1") as HTMLButtonElement;
-const button2 = document.querySelector("#button2") as HTMLButtonElement;
-const button3 = document.querySelector("#button3") as HTMLButtonElement;
-const button4 = document.querySelector("#button4") as HTMLButtonElement;
-function displayQuestion(question: Question) {
-  questionElement.textContent = question.question;
-  questionContainer.appendChild(questionElement);
-  questionContainer.appendChild(buttonDiv);
-  button1.textContent = question.options[0];
-  buttonDiv.appendChild(button1);
-  button2.textContent = question.options[1];
-  buttonDiv.appendChild(button2);
-  button3.textContent = question.options[2];
-  buttonDiv.appendChild(button3);
-  button4.textContent = question.options[3];
-  buttonDiv.appendChild(button4);
-  questionTimer();
-  checkAnswer();
-  /*   const buttons: NodeListOf<HTMLButtonElement> =
-    document.querySelectorAll(".button");
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", () => {});
-  } */
-}
-
-const presentation = document.querySelector(".presentation") as HTMLDivElement;
-
+//Getting form input
 const form = document.querySelector("form") as HTMLFormElement;
-let inputName: any = document.querySelector("#playerName") as HTMLInputElement;
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(inputName.value);
-  localStorage.setItem("playerName", inputName.value);
-  const playerName = localStorage.getItem("playerName");
-  console.log(`${playerName} is the player's name`);
-  const preGame = document.querySelector(".preGame") as HTMLDivElement;
-  const preGameHeading = document.createElement("h2") as HTMLHeadingElement;
-  preGameHeading.classList.add("preGameHeading");
-  preGameHeading.textContent = `Ok ${playerName}, you're about to play the game but before you need to know a few rules:`;
-  preGame.appendChild(preGameHeading);
-  const preGameRules = document.createElement("ul") as HTMLUListElement;
-  preGameRules.classList.add("preGameRules");
-  preGame.appendChild(preGameRules);
-  createLiRules(
-    preGameRules,
-    "You have 20 seconds to answer or your question is automatically false"
-  );
-  createLiRules(
-    preGameRules,
-    "A good answer grants you 10 points and a bad answer takes away 5 points from you."
-  );
-  createLiRules(preGameRules, "Last but not least, enjoy!");
-  const goBtn = document.createElement("button") as HTMLButtonElement;
-  goBtn.classList.add("goBtn");
-  goBtn.textContent = "Ok I get it, let's go";
-  preGame.appendChild(goBtn);
+  let usernameInput = document.querySelector(
+    "#usernameInput"
+  ) as HTMLInputElement;
+  // putting it in local storage
+  localStorage.setItem("username", usernameInput.value);
+  // display the first question
+  /*   presentation.classList.add("hide"); */
   presentation.style.display = "none";
-  preGame.style.display = "flex";
-  goBtn.addEventListener("click", () => {
-    const preGame = document.querySelector(".preGame") as HTMLDivElement;
-    preGame.style.display = "none";
-    questionContainer.style.display = "flex";
-    displayQuestion(questions[currentQuestionIndex]);
-  });
+  questionDiv.style.display = "flex";
+  displayQuestion();
 });
 
-function createLiRules(ulElement: HTMLUListElement, text: string) {
-  const newLi = document.createElement("li") as HTMLLIElement;
-  newLi.textContent = text;
-  ulElement.appendChild(newLi);
+// getting HTML elements of question div
+const question = document.querySelector(".question") as HTMLHeadingElement;
+const button = document.querySelectorAll(".questionButton");
+const questionNumber = document.querySelector(
+  ".questionNumber"
+) as HTMLParagraphElement;
+function displayQuestion() {
+  question.textContent = questionArray[questionIndex].question;
+  for (let i = 0; i < button.length; i++) {
+    button[i].textContent = questionArray[questionIndex].options[i];
+  }
+  questionNumber.textContent = `${questionIndex + 1}/5`;
 }
 
-const buttons: NodeListOf<Element> = document.querySelectorAll(".button");
-const questionResultDiv = document.querySelector(
-  ".question-result"
-) as HTMLDivElement;
-
-const images = document.querySelectorAll("img");
-console.log(images);
-function displayQuestionResult(correctAnswer: string) {
-  questionContainer.style.display = "none";
-  questionResultDiv.style.display = "flex";
-  const answerHeading = document.createElement("h2") as HTMLHeadingElement;
-  answerHeading.classList.add("answerHeading");
-  answerHeading.textContent = "The right answer is:";
-  questionResultDiv.appendChild(answerHeading);
-  const answerDiv = document.createElement("div") as HTMLDivElement;
-  answerDiv.classList.add("answerDiv");
-  questionResultDiv.appendChild(answerDiv);
-
-  const currentImage = images[currentQuestionIndex];
-  currentImage.style.display = "block";
-  answerDiv.appendChild(currentImage);
-  const answerTextDiv = document.createElement("div") as HTMLDivElement;
-  answerTextDiv.classList.add("answerTextDiv");
-  answerDiv.appendChild(answerTextDiv);
-  const textAnswer = document.createElement("h3") as HTMLHeadingElement;
-  textAnswer.textContent = correctAnswer;
-  answerTextDiv.appendChild(textAnswer);
-  const scoreAnswer = document.createElement("p") as HTMLParagraphElement;
-  scoreAnswer.textContent = `Your score is now: ${score}.`;
-  answerTextDiv.appendChild(scoreAnswer);
-  const nextButton = document.createElement("button") as HTMLButtonElement;
-  nextButton.classList.add("nextButton");
-  nextButton.textContent = "Next question";
-  questionResultDiv.appendChild(nextButton);
-  nextButton.addEventListener("click", () => {
-    if (currentQuestionIndex === 5) {
-      questionContainer.style.display = "none";
-      questionResultDiv.style.display = "none";
-      displayFinalResult(score);
-    } else {
-      answerHeading.remove();
-      answerDiv.remove();
-      nextButton.remove();
-      questionResultDiv.style.display = "none";
-      questionContainer.style.display = "flex";
-      displayQuestion(questions[currentQuestionIndex]);
-      questionTimer();
-    }
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", () => {
+    let selectedAnswer = button[i].textContent;
+    displayAnswer(selectedAnswer);
   });
 }
 
-function checkAnswer() {
-  let correctAnswer = questions[currentQuestionIndex].correctAnswer;
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", () => {
-      console.log("button is clicked");
-      /*       console.log(buttons[i].textContent, correctAnswer); */
-      if (buttons[i].textContent === correctAnswer) {
-        /*  console.log(
-          `${buttons[i].textContent} is the right answer. ${correctAnswer} was.`
-        ); */
-        score += 10;
-        displayQuestionResult(correctAnswer);
-        currentQuestionIndex++;
-        buttons[i].textContent = "";
-        correctAnswer = questions[currentQuestionIndex].correctAnswer;
-        /*  console.log(`The next correct answer will be: ${correctAnswer}`); */
-      } else {
-        /*   console.log(
-          `${buttons[i].textContent} is not the right answer. ${correctAnswer} was.`
-        ); */
-        score -= 5;
-        displayQuestionResult(correctAnswer);
-        currentQuestionIndex++;
-
-        buttons[i].textContent = "";
-
-        correctAnswer = questions[currentQuestionIndex].correctAnswer;
-
-        /* console.log(`The next correct answer will be: ${correctAnswer}`); */
-      }
+const answer = document.querySelector(".answer") as HTMLHeadingElement;
+const roundPoints = document.querySelector(
+  ".roundPoints"
+) as HTMLParagraphElement;
+const nextButton = document.querySelector(".nextButton") as HTMLButtonElement;
+function displayAnswer(selectedAnswer) {
+  questionDiv.style.display = "none";
+  answerDiv.style.display = "flex";
+  if (selectedAnswer === questionArray[questionIndex].correctAnswer) {
+    answer.textContent = `You're right, the answer is ${selectedAnswer}.`;
+    playerPoints += 10;
+    roundPoints.textContent = `You won 10 points and you currently have a total of ${playerPoints} points`;
+    numberGoodAnswer++;
+    questionIndex++;
+  } else {
+    answer.textContent = `Nope, the answer was ${questionArray[questionIndex].correctAnswer}.`;
+    playerPoints -= 5;
+    roundPoints.textContent = `You lost 5 points and you currently have a total of ${playerPoints} points.`;
+    questionIndex++;
+  }
+  if (questionIndex > 4) {
+    finalResult();
+  } else {
+    nextButton.addEventListener("click", () => {
+      questionDiv.style.display = "flex";
+      answerDiv.style.display = "none";
+      displayQuestion();
     });
   }
 }
 
-function displayFinalResult(score: number) {
-  const playerName = localStorage.getItem("playerName");
-  const finalResultDiv = document.querySelector(
-    ".final-result-container"
-  ) as HTMLDivElement;
-  finalResultDiv.style.display = "flex";
-  const finalHeading = document.createElement("h2") as HTMLHeadingElement;
-  finalHeading.textContent = `Well, well, well ${playerName}... Let's see how you did... :`;
-  finalResultDiv.appendChild(finalHeading);
-  const scoreDisplay = document.createElement("h3") as HTMLParagraphElement;
-  if (score >= 40) {
-    scoreDisplay.textContent = `Bro what! You scored ${score}! ${score} damn points! You're some China expert or  what? GGWP!!!`;
-  } else if (score >= 20 && score < 40) {
-    scoreDisplay.textContent = `You scored ${score} points! That a great game, well done!`;
-  } else {
-    scoreDisplay.textContent = `You end up with ${score}. It's ok, nobody is an know-it-all! Me neither!`;
-  }
-  finalResultDiv.appendChild(scoreDisplay);
-  const againBtn = document.createElement("button") as HTMLButtonElement;
-  againBtn.textContent = "Play again";
-  finalResultDiv.appendChild(againBtn);
-  /*   againBtn.addEventListener("click", () => {
-    finalResultDiv.style.display = "none";
-    presentation.style.display = "flex";
-  }); */
-}
-
-function questionTimer() {
-  if (questionContainer.style.display === "flex") {
-    /*     setTimeout(() => {
-      setFalseAnswer();
-    }, 19000); */
-    setInterval(() => {
-      decrementByOne();
-    }, 1000);
-  }
-}
-const secondsDisplay = document.querySelector(
-  ".secondsDisplay"
+const numberRightAnswer = document.querySelector(
+  ".numberRightAnswer"
+) as HTMLHeadingElement;
+const pointsEarned = document.querySelector(
+  ".pointsEarned"
 ) as HTMLParagraphElement;
-
-function setFalseAnswer() {
-  starterTimer = 20;
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].textContent = "";
-  }
-  const button1 = document.querySelector("#button1") as HTMLButtonElement;
-  button1.click();
-}
-
-let starterTimer: number = 20;
-
-function decrementByOne() {
-  if (secondsDisplay.textContent === "0") {
-    return;
+const playerName = localStorage.getItem("username");
+function finalResult() {
+  answerDiv.style.display = "none";
+  finalResultDiv.style.display = "flex";
+  if (numberGoodAnswer === 5) {
+    numberRightAnswer.textContent = `You answered right on all questions! Congrats ${playerName}!`;
   } else {
-    secondsDisplay.textContent = `${starterTimer--}`;
+    numberRightAnswer.textContent = `${playerName}, you answered right on ${numberGoodAnswer}/5 questions!`;
+  }
+  if (playerPoints < 30) {
+    pointsEarned.textContent = `You got ${playerPoints} points in that game.`;
+  } else {
+    pointsEarned.textContent = `You won ${playerPoints} points in that game!`;
   }
 }
+
+const againButton = document.querySelector(".playAgain") as HTMLButtonElement;
+againButton.addEventListener("click", () => {
+  location.reload();
+});
